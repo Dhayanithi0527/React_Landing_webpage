0b1f77a3201462a083e55a4d288d51c1e05d7978
Dhayanithi0527 <dhayanithi0527@gmail.com>
2023-07-01 15:42:31 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// import { BrowserRouter,Routes,Route } from 'react-router-dom';
+// import Home from './pages/Home'
+// import Contact  from './pages/Contact'
+// import About  from './pages/About'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div class="container" >
+            
+        <h3><button class="head">Shop</button></h3>
+        
+
+        <ul class="headings">
+            <li><a href="">Home</a></li>
+            <li><a href="">Mobile</a> </li>
+            <li><a href="">Laptops</a></li>
+            <li><a href="">Gadgets</a></li>
+            <li><a href="">Kitchen Applicanes</a></li>
+            <li><a href="">Steel</a></li>
+            <li><a href="">Clothes</a></li>
+            <li><a href="">Grocery's</a></li>
+            <button class="Login">Login</button>
+            <button class="Login">Cancel</button>
+            
+        </ul>
+        
+        
+      </div>
   );
 }
 
-export default App;
